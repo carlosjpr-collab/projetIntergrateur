558966443261ad5eb93b02a82fdf95407dadc17e
helcmanocki <33450032+helcmanocki@users.noreply.github.com>
2019-01-21 10:43:57 +0100

    train_label_class
    Modifie la normalisation des labels

---
+++ label.py
@@ -0,0 +1,29 @@
+import numpy as np
+
+
+
+label = np.load('test_labels_0_10_25.npy')
+#label1 = np.load('test_labels_0_10_25.npy')
+
+for line in range(0,42805):
+	index = np.zeros((5))
+	index=label[line]
+	if (index[0] == 1) :
+		label[line, 0] = 1
+	if (index[1] == 1) :
+		label[line, 1] = 2
+	if (index[2] == 1) :
+		label[line, 2] = 3
+	if (index[3] == 1) :
+		label[line, 3] = 4
+	if (index[4] == 1) :
+		label[line, 4] = 5
+	
+#	print(label[line])
+#	print(label1[line])
+#	print('----')
+
+
+np.save('train_label_class.npy', label)
+
+
