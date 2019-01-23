2a4997532015d96010067a3d08e932195ea63812
helcmanocki <33450032+helcmanocki@users.noreply.github.com>
2019-01-23 17:38:32 +0100

    Update label.py

--- label.py
+++ label.py
@@ -1,23 +1,39 @@
 import numpy as np
 
-# Modifie la nomenclature des labels du dataset de test
+# Modifie la nomenclature des labels du dataset de test et train
 
-label = np.load('test_labels_0_10_25.npy')
-#label1 = np.load('test_labels_0_10_25.npy')
+label_train = np.load('INSA_Train/train_labels_0_10_25.npy')
+label_test = np.load('INSA_Test/test_labels_0_10_25.npy')
 
 for line in range(0,42805):
 	index = np.zeros((5))
-	index=label[line]
+	index=label_train[line]
 	if (index[0] == 1) :
-		label[line, 0] = 1
+		label_train[line, 0] = 1
 	if (index[1] == 1) :
-		label[line, 1] = 2
+		label_train[line, 1] = 2
 	if (index[2] == 1) :
-		label[line, 2] = 3
+		label_train[line, 2] = 3
 	if (index[3] == 1) :
-		label[line, 3] = 4
+		label_train[line, 3] = 4
 	if (index[4] == 1) :
-		label[line, 4] = 5
+		label_train[line, 4] = 5
 
-np.save('train_label_class.npy', label)
+np.save('INSA_Train/train_label_class.npy', label_train)
+
+for line in range(0,42805):
+	image = np.zeros((5))
+	image=label_test[line]
+	if (image[0] == 1) :
+		label_test[line, 0] = 1
+	if (image[1] == 1) :
+		label_test[line, 1] = 2
+	if (image[2] == 1) :
+		label_test[line, 2] = 3
+	if (image[3] == 1) :
+		label_test[line, 3] = 4
+	if (image[4] == 1) :
+		label_test[line, 4] = 5
+
+np.save('INSA_Test/test_label_class.npy', label_test)
 
