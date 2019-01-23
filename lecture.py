570e75ddc17aaf0469bf4da7d1fee88ed9e6a358
helcmanocki <33450032+helcmanocki@users.noreply.github.com>
2019-01-23 17:18:25 +0100

    Update lecture.py

--- lecture.py
+++ lecture.py
@@ -1,14 +1,15 @@
+
 import numpy as np
 import matplotlib.pyplot as plt
 
-array_loaded = np.load ('test.npy')
-print(array_loaded.shape)
+# Ce script affiche les images du fichier passé en entré sans les modifiers
 
-"""
-for i in range(0,42805):
-#for i in range(0,2000):
+array_loaded = np.load ('INSA_Test/test_RGB_0_10_25.npy')
+len1=(array_loaded.shape[0])-1
+
+
+for i in range(0,len1):
 	image = np.zeros((32,32,3))	
 	image = array_loaded[i]
 	plt.imshow(image, alpha=0.75, interpolation='spline36')
 	plt.show()
-"""
