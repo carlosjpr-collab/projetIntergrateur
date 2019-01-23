8552bb492e693caaf7bc1a2061e64397172e5f57
helcmanocki <33450032+helcmanocki@users.noreply.github.com>
2019-01-23 17:14:32 +0100

    Update projet.py

--- projet.py
+++ projet.py
@@ -1,31 +1,26 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import matplotlib.image as mpimg
-import matplotlib.pylab as plt
 
 
-
-array_loaded = np.load ('test_RGB_0_10_25.npy')
-#label = np.load('test_labels_0_10_25.npy')
+# Ce script permet d'afficher et de sauver les images en modifiant les couleurs
 
 
-#Finding the value of the max pixel 
+#array_loaded = np.load ('INSA_Test/test_RGB_0_10_25.npy')
+array_loaded = np.load ('INSA_Test/test_image2.npy')
+len1=(array_loaded.shape[0])-1
+
+
+# Trouve la valeur max des pixels 
 def pixel_max (line):
 	image = np.zeros((32,32,3))
 	for m in range(0,32) :
 		for n in range(0,32) :
 			image[m,n]=array_loaded[line, m,n]
 	max = image.max()
-	# red
-	#red = image[..., 0].max()
-	# green
-	#green = image[..., 1].max()
-	# blue
-	#blue = image[..., 2].max()
 	return max
 
 
-#Improve the colors of the image
+# Ameliore les couleurs de l image
 def img_improvement (line, max) :
 	image = np.zeros((32,32,3))
 	for k in range(0,32) :
@@ -38,15 +33,15 @@ def img_improvement (line, max) :
 	return image
 
 
-		
-for i in range(0,42805):
-#for i in range(0,2000):
+
+# Appel des fonctions	
+for i in range(0,len1):
 	image = np.zeros((32,32,3))
 	max = pixel_max(i)
 	image = img_improvement(i,max)
 	array_loaded[i]=image
-	#plt.imshow(array_loaded[i], alpha=0.75, interpolation='spline36')
-	#plt.show()
+	plt.imshow(array_loaded[i], alpha=0.75, interpolation='spline36')
+	plt.show()
 
-np.save('test.npy', array_loaded)
-
+	# on sauve les nouvelles images dans un fichier
+	#np.save('INSA_Test/test.npy', array_loaded)
