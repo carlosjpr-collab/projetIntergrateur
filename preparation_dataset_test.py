297183e5e5273a02fbf9fe4452d5d5756a5a0c74
helcmanocki <33450032+helcmanocki@users.noreply.github.com>
2019-01-23 19:15:26 +0100

    add label

--- preparation_dataset_test.py
+++ preparation_dataset_test.py
@@ -1,10 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-# Ce script permet de copier le dataset test en supprimant les images noires et le fragmente ensuite en 3
+# Ce script permet de copier le dataset test en supprimant les images noires et le fragmente ensuite en 3 avec les labels
 
 array_loaded = np.load ('INSA_Test/test_RGB_0_10_25.npy')
-
+label_loaded = np.load ('INSA_Test/test_label_class.npy')
 
 len1=(array_loaded.shape[0])-1
 counter = 0
@@ -22,8 +22,7 @@ def nb_images_noires (l1, counter):
 	return counter
 
 # créé un nouveau dataset sans images noires
-def create_array (l1,l2):
-	new_array=np.zeros((l2, 32,32,3))
+def create_array (l1,l2, array, label):
 	image = np.zeros((32,32,3))
 	index = 0
 	for line in range (0,l1):
@@ -32,9 +31,10 @@ def create_array (l1,l2):
 				image[i,j]=array_loaded[line,i,j]
 		max = image.max()
 		if (max != 0.0):
-			new_array[index]=image
+			array[index]=image
+			label[index]=label_loaded[line]
 			index = index + 1
-	return new_array
+	return array, label
 
 
 # récupère le nb d images noires
@@ -47,10 +47,12 @@ if (result != 0):
 	print(len2)
 	# création d'une nouvelle image numpy
 	new_array = np.zeros((len2, 32,32,3)) 
-	new_array = create_array (len1,len2)
+	new_label = np.zeros((len2, 5)) 
+	new_array, new_label = create_array (len1,len2, new_array, new_label)
 
-	# on la sauve dans un fichier
+	# on les sauve dans des fichiers
 	np.save('INSA_Test/new_test_RGB.npy', new_array)
+	np.save('INSA_Test/new_label.npy', new_label)
 
 	# pour fragmenter en 3 le nouveau dataset
 	array1, array2, array3 = np.split(new_array,3)
@@ -59,9 +61,14 @@ if (result != 0):
 	np.save('INSA_Test/test_image2.npy',array2)
 	np.save('INSA_Test/test_image3.npy',array3)
 	print('Decoupage du dataset')
-	
 
+	# decoupage des labels
+	label1, label2, label3 = np.split(new_label,3)
+	np.save('INSA_Test/test_label1.npy',label1)
+	np.save('INSA_Test/test_label2.npy',label2)
+	np.save('INSA_Test/test_label3.npy',label3)
+	print('Decoupage des labels')
+	
 #sinon
 else :
 	print("pas d images noires dans le dataset")
-
