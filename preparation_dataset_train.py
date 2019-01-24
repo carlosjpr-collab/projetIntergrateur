48028d81f2c019ab6fdfe78f9c1ed5f70fda6bcd
ElyesKhama <khamassi@etud.insa-toulouse.fr>
2019-01-24 12:30:33 +0100

    update

--- preparation_dataset_train.py
+++ preparation_dataset_train.py
@@ -9,47 +9,58 @@ label_loaded = np.load ('INSA_Train/train_label_class.npy')
 len1=(array_loaded.shape[0])-1
 counter = 0
 
+"""
 # la fonction retourne le nombre d'images noires dans la dataset
-def nb_images_noires (l1, counter):
+def images_noires (l1, counter):
 	image = np.zeros((32,32,3))
 	for line in range (0,l1):
-		for m in range(0,32) :
-			for n in range(0,32) :
-				image[m,n]=array_loaded[line, m,n]
-		maximum = image.max()
-		if (maximum == 0.0):
+		if (np.count_nonzero(array_loaded[line])==0):
 			counter = counter+1
 	return counter
 
-# créé un nouveau dataset sans images noires
+# cree un nouveau dataset sans images noires
 def create_array (l1,l2, array, label):
 	image = np.zeros((32,32,3))
 	index = 0
-	for line in range (0,l1):
-		for i in range(0,32) :
-			for j in range(0,32) :
-				image[i,j]=array_loaded[line,i,j]
-		max = image.max()
-		if (max != 0.0):
-			array[index]=image
-			label[index]=label_loaded[line]
-			index = index + 1
+	while(index != (l2-1)):
+          for line in range (0,l1):
+			if (np.count_nonzero(array_loaded[line])!=0):
+				array[index]=image
+				label[index]=label_loaded[line]
+				index = index + 1
 	return array
+"""
+
+data1, data2, data3 = np.split(array_loaded, 3)
+
+def images_noires(array):
+	index_images_noires = []
+	for i in range(0, array.shape[0]):
+		if(np.count_nonzero(array[i])==0):
+			index_images_noires.append(i)
+	return index_images_noires
+
+index_delete1 = images_noires(data1)
+index_delete2 = images_noires(data2)
+index_delete3 = images_noires(data3)
+
+mask = np.ones(data1.shape[0], dtype=bool)
+mask[index_delete1]=False
+data1_clean = data1[mask]
 
 
-# récupère le nb d images noires
+"""
+# recupere le nb d images noires
 result = nb_images_noires(len1,counter)
-
-# si on a detecté des images noires
+# si on a detecte des images noires
 if (result != 0):
 	len2=len1-result
 	print("nb d image de la nouvelle array")
 	print(len2)
-	# création d'une nouvelle image numpy
-	new_array = np.zeros((len2, 32,32,3)) 
-	new_label = np.zeros((len2, 5)) 
-	new_array, new_label = create_array (len1,len2, new_array, new_label)
-
+	# creation d'une nouvelle image numpy
+	new_array = np.zeros((len2/3, 32,32,3)) 
+	new_label = np.zeros((len2/3, 5)) 
+	new_array, new_label = create_array (len1,len2/3, new_array, new_label)
 	# on la sauve dans un fichier
 	np.save('INSA_Train/new_train_RGB.npy', new_array)
 	np.save('INSA_Train/new_train_label.npy', new_label)
@@ -58,4 +69,4 @@ if (result != 0):
 #sinon
 else :
 	print("pas d images noires dans le dataset")
-
+"""
